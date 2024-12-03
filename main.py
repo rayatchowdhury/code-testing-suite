b65407c6af23a9328ff9fb0340c2a14a0d2ecfa3
Rayat Chowdhury <rayated.ray@gmail.com>
2024-12-03 08:41:48 +0600

    Set application icon and remove logo image from main window header

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import Qt
 from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtGui import QIcon
 from views.main_window import MainWindow
 
 
@@ -11,6 +12,9 @@ def main():
     QApplication.setAttribute(Qt.AA_ShareOpenGLContexts)  # Required for WebEngine
     
     app = QApplication(sys.argv)
+    
+    # Set application icon
+    app.setWindowIcon(QIcon("resources/icons/app_icon.png"))
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
